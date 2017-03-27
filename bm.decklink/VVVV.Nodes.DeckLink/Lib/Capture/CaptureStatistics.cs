88a294f08568e6f54370699e6455a231527dd9ed
julien vulliet <julien@mrvux.com>
2017-03-27 17:13:57 +0200

    [Node] Add capture statistics struct and unzip node to go with it

---
+++ bm.decklink/VVVV.Nodes.DeckLink/Lib/Capture/CaptureStatistics.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVVV.DeckLink
+{
+    /// <summary>
+    /// Contains capture statistics for a device
+    /// </summary>
+    public struct CaptureStatistics
+    {
+        /// <summary>
+        /// Number of frames captures by the device
+        /// </summary>
+        public int FramesCapturedCount;
+
+        /// <summary>
+        /// Number of frames uploaded to the graphics card
+        /// </summary>
+        public int FramesCopiedCount;
+
+        /// <summary>
+        /// Current size of the queued frames, if applicable
+        /// </summary>
+        public int FramesQueueSize;
+
+        /// <summary>
+        /// Number of times the current frame has been presented to the screen
+        /// </summary>
+        public int CurrentFramePresentCount;
+
+        /// <summary>
+        /// Number of frames that got dropped
+        /// </summary>
+        public int FramesDroppedCount;
+
+        /// <summary>
+        /// Delay between two frames (this is calculated as soon as a new frame is received, so that should be framerate
+        /// </summary>
+        public double DelayBetweenFrames;
+
+        /// <summary>
+        /// Delay between two update calls, please note that it does not say if frame got update or not
+        /// </summary>
+        public double DelayBetweenTextureUpdates;
+
+        /// <summary>
+        /// Delay between the time a frame got acquired and time is has been assigned for presentation
+        /// </summary>
+        public double CurrentDelay;
+    }
+}
---
+++ bm.decklink/VVVV.Nodes.DeckLink/Nodes/UnzipCaptureStatisticsNode.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.DeckLink.Nodes
+{
+    [PluginInfo(Name = "Unzip", Category = "DeckLink", Version = "CaptureStatistics", Author = "vux", Tags = "blackmagic, capture",
+        Help ="Unzips all statistics for a decklink capture")]
+    public class UnzipStatisticsNode : IPluginEvaluate
+    {
+        [Input("Input")]
+        protected ISpread<CaptureStatistics> input;
+
+        [Output("Frames Captured Count")]
+        protected ISpread<int> framesCapturedCount;
+
+        [Output("Frames Copied Count")]
+        protected ISpread<int> framesCopiedCount;
+
+        [Output("Frames Queue Size")]
+        protected ISpread<int> framesQueueSize;
+
+        [Output("Current Frame Present Count")]
+        protected ISpread<int> currentFramePresentCount;
+
+        [Output("Frames Dropped Count")]
+        protected ISpread<int> framesDroppedCount;
+
+        [Output("Delay between frames")]
+        protected ISpread<double> delayBetweenFrames;
+
+        [Output("Delay between texture updates")]
+        protected ISpread<double> delayBetweenTextureUpdates;
+
+        [Output("Current Delay")]
+        protected ISpread<double> currentDelay;
+
+        public void Evaluate(int SpreadMax)
+        {
+            this.currentDelay.SliceCount = SpreadMax;
+            this.currentFramePresentCount.SliceCount = SpreadMax;
+            this.delayBetweenFrames.SliceCount = SpreadMax;
+            this.delayBetweenTextureUpdates.SliceCount = SpreadMax;
+            this.framesCapturedCount.SliceCount = SpreadMax;
+            this.framesCopiedCount.SliceCount = SpreadMax;
+            this.framesDroppedCount.SliceCount = SpreadMax;
+            this.framesQueueSize.SliceCount = SpreadMax;
+
+            for (int i = 0; i < SpreadMax; i++)
+            {
+                CaptureStatistics stats = this.input[i];
+                this.currentDelay[i] = stats.CurrentDelay;
+                this.currentFramePresentCount[i] = stats.CurrentFramePresentCount;
+                this.delayBetweenFrames[i] = stats.DelayBetweenFrames;
+                this.delayBetweenTextureUpdates[i] = stats.DelayBetweenTextureUpdates;
+                this.framesCapturedCount[i] = stats.FramesCapturedCount;
+                this.framesCopiedCount[i] = stats.FramesCopiedCount;
+                this.framesDroppedCount[i] = stats.FramesDroppedCount;
+                this.framesQueueSize[i] = stats.FramesQueueSize;
+            }
+        }
+    }
+}
