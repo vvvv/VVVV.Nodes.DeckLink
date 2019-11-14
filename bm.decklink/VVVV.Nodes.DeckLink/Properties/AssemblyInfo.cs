a8dad480ceeb9919564bff7370b62a639b689669
Guido Schmidt <guido@nsynk.de>
2019-11-14 15:55:53 +0100

    EDIT/bm.decklink/AssemblyInfo.cs: update version

--- bm.decklink/VVVV.Nodes.DeckLink/Properties/AssemblyInfo.cs
+++ bm.decklink/VVVV.Nodes.DeckLink/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0-beta-6-g9f6bacb")]
+[assembly: AssemblyInformationalVersion("1.1.0-beta-8-g8280f33")]
